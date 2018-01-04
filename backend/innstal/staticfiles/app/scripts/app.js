9493873798ee0c49193eaf207a10e29a262a540c
Nija P <nijap@techversantinfotech.com>
2018-01-04 18:55:34 +0530

    Changes in template

---
+++ backend/innstal/staticfiles/app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appNameApp
+ * @description
+ * # appNameApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appNameApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/myroute', {
+        templateUrl: 'views/myroute.html',
+        controller: 'MyrouteCtrl',
+        controllerAs: 'myroute'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ backend/innstal/staticfiles/app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appNameApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the appNameApp
+ */
+angular.module('appNameApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
